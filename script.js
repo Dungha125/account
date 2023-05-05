daac250faa96d35af18f83f3162e89bf4d276f4e
Dungha125 <dungha122405@gmail.com>
2023-05-05 10:27:09 +0700

    Dungha

---
+++ script.js
@@ -0,0 +1,11 @@
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".dangnhap");
+const registerLink = document.querySelector(".dangki");
+
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+})
